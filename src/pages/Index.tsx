85b7927b826323c1043db082be8773b0155d45c4
huzhengen <huzhengen@163.com>
2023-07-10 21:52:49 +0800

    添加路由

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+function Index() {
+
+
+
+  return (
+    <>
+      这是首页！！！
+    </>
+  )
+}
+
+export default Index
---
+++ src/router/router.tsx
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Index from "../pages/Index";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/blog",
+    element: <div>blog</div>,
+  },
+]);
